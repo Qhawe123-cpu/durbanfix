ee8d8c95ac3c0a55d77d33908a328b710f2ac0a2
Qhawe123-cpu <qhawemgwili66@gmail.com>
2026-04-02 10:28:58 +0200

    Added reports page and improved UI

--- fix/urls.py
+++ fix/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('report/', views.report_issue, name='report'),
+
+    path('reports/', views.all_reports, name='all_reports'),
+    path('report/<int:id>/', views.report_detail, name='report_detail'),
 ]
--- fix/views.py
+++ fix/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Report
 
 def home(request):
@@ -11,19 +10,31 @@ def home(request):
 
 def report_issue(request):
     if request.method == 'POST':
-        issue_type = request.POST.get('issue_type')
-        location = request.POST.get('location')
-        description = request.POST.get('description')
+        issue = request.POST['issue']
+        location = request.POST['location']
+        description = request.POST['description']
 
-        if issue_type and location and description:
-            Report.objects.create(
-                issue_type=issue_type,
-                location=location,
-                description=description
-            )
-            messages.success(request, "✅ Issue reported successfully!")
-            return redirect('home')
-        else:
-            messages.error(request, "❌ Please fill in all fields.")
+        Report.objects.create(
+            issue_type=issue,
+            location=location,
+            description=description
+        )
+
+        return redirect('home')
 
     return render(request, 'report.html')
+
+
+
+
+
+def all_reports(request):
+    from .models import Report
+    reports = Report.objects.all()
+    return render(request, 'all_reports.html', {'reports': reports})
+
+
+def report_detail(request, id):
+    from .models import Report
+    report = Report.objects.get(id=id)
+    return render(request, 'report_detail.html', {'report': report})
